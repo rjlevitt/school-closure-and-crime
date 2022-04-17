72255020fedbb131528d87587d94d7394a3cbf98
vulpes37 <rjlevitt37@gmail.com>
2022-04-16 19:40:01 -0600

    read and merge incidents, offenders, agencies

--- code/nibrs_explore.py
+++ code/nibrs_explore.py
@@ -8,12 +8,14 @@ def get_data_path():
     return data_path
 
 
-def read_nibrs_data(data_path):
+def read_merge_nibrs(data_path):
     incident = pd.read_csv(os.path.join(data_path, 'NIBRS_incident.csv'))
     offender = pd.read_csv(os.path.join(data_path, 'NIBRS_OFFENDER.csv'))
-    return incident, offender
+    agency = pd.read_csv(os.path.join(data_path, 'agencies.csv'))
+    df = incident.merge(offender, how='inner', on=['INCIDENT_ID', 'DATA_YEAR'])
+    df = df.merge(agency, how='inner', on='AGENCY_ID')
+    return df
 
 
 if __name__ == '__main__':
-    incident, offender = read_nibrs_data(data_path=get_data_path())
-    df = incident.merge(offender, how='inner', on=['INCIDENT_ID', 'DATA_YEAR'])
+    dat = read_merge_nibrs(data_path=get_data_path())
